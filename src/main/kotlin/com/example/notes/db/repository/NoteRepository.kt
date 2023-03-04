354c3ab358eead830a079caf61475495140192ba
ashabelskii <ashabelskii@neoflex.ru>
2023-03-04 15:36:07 +0300

    add get notes by categoryId

--- src/main/kotlin/com/example/notes/db/repository/NoteRepository.kt
+++ src/main/kotlin/com/example/notes/db/repository/NoteRepository.kt
@@ -3,4 +3,7 @@ package com.example.notes.db.repository
 import com.example.notes.db.entity.Note
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface NoteRepository : JpaRepository<Note, Long>
+interface NoteRepository : JpaRepository<Note, Long> {
+
+    fun findNotesByCategoryId(id: Long): List<Note>
+}
