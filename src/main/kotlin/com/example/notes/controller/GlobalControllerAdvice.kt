c63787105d3a4325d9969398dc328c74d2f2555d
ashabelskii <owgrant18@gmail.com>
2023-03-12 02:31:14 +0300

    add logging

--- src/main/kotlin/com/example/notes/controller/GlobalControllerAdvice.kt
+++ src/main/kotlin/com/example/notes/controller/GlobalControllerAdvice.kt
@@ -1,5 +1,6 @@
 package com.example.notes.controller
 
+import mu.KotlinLogging
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
@@ -7,9 +8,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 
 @ControllerAdvice
 class GlobalControllerAdvice {
+    private val logger = KotlinLogging.logger {}
 
     @ExceptionHandler(Exception::class)
     fun handleAllExceptions(e: Exception): ResponseEntity<String> {
+        logger.error(e) { e.message }
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
     }
 }
