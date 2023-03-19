440da26b48a3894aa8b5e00a12f21c48134d6e63
ashabelskii <owgrant18@gmail.com>
2023-03-19 22:49:32 +0300

    add ckeditor

--- src/main/resources/static/js/note.js
+++ src/main/resources/static/js/note.js
@@ -1,6 +1,27 @@
+ClassicEditor
+    .create(document.querySelector('.editor'), {})
+    .then(editor => {
+        window.editor = editor;
+        const toolbarElement = editor.ui.view.toolbar.element;
+
+        editor.enableReadOnlyMode('editor');
+        toolbarElement.style.display = 'none';
+
+        editor.on('change:isReadOnly', (evt, propertyName, isReadOnly) => {
+            if (isReadOnly) {
+                toolbarElement.style.display = 'none';
+            } else {
+                toolbarElement.style.display = 'flex';
+            }
+        });
+    })
+    .catch(error => {
+        console.error(error);
+    });
+
 function updateNote(id) {
     let name = document.getElementById("name-note").value;
-    let description = document.getElementById("content-note").value;
+    let description = editor.getData();
 
     axios.put('/v1/notes/' + id, {
         name: name,
@@ -17,4 +38,10 @@ function notifySuccessfulSaving() {
     const notifySuccess = document.getElementById('notifySuccess');
     const toast = new bootstrap.Toast(notifySuccess);
     toast.show();
+}
+
+function changeReadOnlyStatus() {
+    editor.isReadOnly
+        ? editor.disableReadOnlyMode("editor")
+        : editor.enableReadOnlyMode("editor")
 }
