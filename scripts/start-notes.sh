14b5fd12d29cacd33bd86f3b624075d53c3c04f3
ashabelskii <owgrant18@gmail.com>
2023-03-21 19:11:10 +0300

    add sh for start app

---
+++ scripts/start-notes.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+java -version
+java -jar -Dspring.datasource.url="jdbc:postgresql://localhost:5432/notes-db" -Dspring.datasource.username="postgres" -Dspring.datasource.password="postgres" ./notes.jar
